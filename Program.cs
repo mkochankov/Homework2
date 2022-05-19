8c5105154df9f39af107824e32b7ad539bd2b575
Михаил Кочанков <m-ko4ankov-98@yandex.ru>
2022-05-19 20:14:40 +0300

    Homework2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(number);
+num = num.Remove(0,1);
+num = num.Remove(1,1);
+Console.WriteLine(num);
